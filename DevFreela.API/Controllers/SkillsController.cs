3c8331da8242ef9eda851e3454910eeb0fca17f6
Jonatan Silva <natanp.silva@hotmail.com>
2023-04-26 15:49:55 -0300

    criação das controllers

---
+++ DevFreela.API/Controllers/SkillsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DevFreela.API.Controllers
+{
+    [Route("api/skills")]
+    public class SkillsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
---
+++ DevFreela.API/Controllers/UsersController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DevFreela.API.Controllers
+{
+    [Route("api/users")]
+    public class UsersController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
