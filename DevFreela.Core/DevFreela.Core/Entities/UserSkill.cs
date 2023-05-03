c05508418830f4be86fd8be8c267087c9c52a110
Jonatan Silva <natanp.silva@hotmail.com>
2023-05-03 16:29:20 -0300

    criação das classes e suas propriedades

---
+++ DevFreela.Core/DevFreela.Core/Entities/UserSkill.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFreela.Core.Entities
+{
+    /*
+     * essas classe representa o relacionamento n - n entre usuários e skills
+     */
+    public class UserSkill : BaseEntity
+    {
+        public UserSkill(int idUser, int idSkill)
+        {
+            IdUser = idUser;
+            IdSkill = idSkill;
+        }
+
+        public int IdUser { get; private set; }
+        public int IdSkill { get; private set; }
+    }
+}
