ad9f3ebaaa05f10ecaeec10176bf10acd49e5b7a
Jonatan Silva <natanp.silva@hotmail.com>
2023-11-08 23:26:21 -0300

    Inclusão do projeto de testes unitários com XUnit e método de teste TestIfProjectStartWorks para classe Project

---
+++ DevFreela.UnitTests/Core/Entities/ProjectTests.cs
@@ -0,0 +1,22 @@
+﻿using DevFreela.Core.Entities;
+using DevFreela.Core.Enums;
+
+namespace DevFreela.UnitTests.Core.Entities
+{
+    public class ProjectTests
+    {
+        [Fact]
+        public void TestIfProjectStartWorks()
+        {
+            //Arrange
+            var project = new Project("Teste de Start Project", "Testar se o método Start funciona", 1, 2, 1000);
+
+            //Act
+            project.Start();
+
+            //Assert
+            Assert.Equal(ProjectStatusEnum.InProgress, project.Status);
+            Assert.NotNull(project.StartedAt);
+        }
+    }
+}
